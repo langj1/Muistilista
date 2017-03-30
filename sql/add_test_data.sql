f765ec6abc9949f3a71a62d7e1a0193be55275da
Jimi <jimi8.lang@gmail.com>
2017-03-30 21:38:47 +0300

    n

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Kayttaja (tunnus, salasana) VALUES('Jonne', 'Ennoj')
-INSERT INTO Luokka (id, Kayttaja, nimi) VALUES('1', 'Jonne', 'Ostokset')
+INSERT INTO Kayttaja (tunnus, salasana) VALUES('Jonne', 'Ennoj');
+INSERT INTO Luokka (id, Kayttaja, nimi) VALUES('1', 'Jonne', 'Ostokset');
