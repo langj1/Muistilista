e24c2a003c24a77993314c30d705f50a1b38c24d
Jimi <jimi8.lang@gmail.com>
2017-03-30 21:29:46 +0300

    n

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,27 +1,27 @@
 CREATE TABLE Kayttaja(
-	tunnus varchar(50) PRIMARY KEY,
-	salasana varchar(50) NOT NULL,
+tunnus varchar(50) PRIMARY KEY,
+salasana varchar(50) NOT NULL,
 );
 
 CREATE TABLE Tehtava(
-	id SERIAL PRIMARY KEY,
-	kayttaja varchar(50) REFERENCES Kayttaja(tunnus),
-	nimi varchar(50) NOT NULL,
-	tarkeys INTEGER NOT NULL,
-	lisatieto varchar(500)
+id SERIAL PRIMARY KEY,
+kayttaja varchar(50) REFERENCES Kayttaja(tunnus),
+nimi varchar(50) NOT NULL,
+tarkeys INTEGER NOT NULL,
+lisatieto varchar(500)
 );
 
 CREATE TABLE Luokka(
-	id SERIAL PRIMARY KEY,
-	kayttaja varchar(50) REFERENCES Kayttaja(tunnus),
-	nimi varchar(50) NOT NULL,
+id SERIAL PRIMARY KEY,
+kayttaja varchar(50) REFERENCES Kayttaja(tunnus),
+nimi varchar(50) NOT NULL,
 );
 
 CREATE TABLE Luokitus(
-	luokka INTEGER REFERENCES Luokka(id),
-	tehtava INTEGER REFERENCES Tehtava(id),
+luokka INTEGER REFERENCES Luokka(id),
+tehtava INTEGER REFERENCES Tehtava(id),
 );
 
 CREATE TABLE Ylaluokka(
-	luokka INTEGER REFERENCES Luokka(id)
+luokka INTEGER REFERENCES Luokka(id)
 );
