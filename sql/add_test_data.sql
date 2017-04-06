44babdfb5d33519421bc428c4cfd15d788a79704
Jimi <jimi8.lang@gmail.com>
2017-04-06 21:10:16 +0300

    n

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO Kayttaja (tunnus, salasana) VALUES('Jonne', 'Ennoj');
-INSERT INTO Luokka (id, Kayttaja, nimi) VALUES('1', 'Jonne', 'Ostokset');
-INSERT INTO Tehtava (id,kayttaja,nimi,tarkeys,lisatieto) VALUES('2','Jonne','Osta maitoa',3,'Maito loppu');
+INSERT INTO Luokka (kayttaja, nimi) VALUES('Jonne', 'Ostokset');
+INSERT INTO Tehtava (kayttaja,nimi,tarkeys,lisatieto) VALUES('Jonne','Osta maitoa',3,'Maito loppu');
