da1406af3457e26513a90661f10a7df0dc4731a6
Jimi <jimi8.lang@gmail.com>
2017-04-06 21:05:30 +0300

    n

--- app/controllers/tehtavat_controller.php
+++ app/controllers/tehtavat_controller.php
@@ -13,10 +13,24 @@ class TehtavaController extends BaseController{
 
 		$params = $_POST;
 
+		$tarkeys = $params['tarkeys'];
+
+		if($tarkeys == 'option1'){
+			$tarkeys = 1;
+		} else if($tarkeys == 'option2'){
+			$tarkeys = 2;
+		} else if($tarkeys == 'option3'){
+			$tarkeys = 3;
+		} else if($tarkeys == 'option4'){
+			$tarkeys = 4;
+		} else if($tarkeys == 'option5'){
+			$tarkeys = 5;
+		} 
+
 		$tehtava = new Tehtava(array(
 			'nimi' => $params['nimi'],
 			'luokka' => $params['luokka'],
-			'tarkeys' => $params['tarkeys'],
+			'tarkeys' => $tarkeys,
 			'lisatieto' => $params['lisatieto'],
 			'kayttaja' => 'Jonne'
 			));
