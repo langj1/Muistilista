dfe91fc1c83ee2bef810a736bdbcb75196823443
Jimi <jimi8.lang@gmail.com>
2017-04-06 23:37:22 +0300

    Korjaus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,3 +1,1 @@
 INSERT INTO Kayttaja (tunnus, salasana) VALUES('Jonne', 'Ennoj');
-INSERT INTO Luokka (kayttaja, nimi) VALUES('Jonne', 'Ostokset');
-INSERT INTO Tehtava (kayttaja,nimi,tarkeys,lisatieto) VALUES('Jonne','Osta maitoa',3,'Maito loppu');
