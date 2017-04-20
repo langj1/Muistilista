07c20c5f71a4beaeb54adb56310ad2bf124f21ec
Jimi <jimi8.lang@gmail.com>
2017-04-20 21:24:41 +0300

    kirjautumista muokattu

--- app/models/kayttaja.php
+++ app/models/kayttaja.php
@@ -7,4 +7,19 @@
    	public function__construct($attributes){
    		parent::__construct($attributes);
    	}
+
+   	public static function authenticate($tunnus, $salasana){
+   		$query = DB:connection()->prepare('SELECT * FROM Kayttaja WHERE tunnus = :tunnus AND salasana = :salasana LIMIT 1');
+   		$query->execute(zrrzy('name'=>$name,'password'=>$password));
+   		$row = $query->fetch();
+   		if($row){
+   			$kayttaja = new Kayttaja(array(
+   				'tunnus' => $row['tunnus'],
+   				'salasana' => $row['salasana']
+   				));
+   			return $kayttaja;
+   		}else{
+   			return null;
+   		}
+   	}
    }
