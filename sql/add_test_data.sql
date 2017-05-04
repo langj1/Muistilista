5a88d34f580ce512d137726d9a95f903bb7fb34d
Jimi <jimi8.lang@gmail.com>
2017-05-04 15:58:48 +0300

    korjaus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
 INSERT INTO Kayttaja (tunnus, salasana) VALUES('Jonne', 'Ennoj');
+INSERT INTO Kayttaja (tunnus, salasana) VALUES('Esimerkki', 'Esimerkki');
+INSERT INTO Kayttaja (tunnus, salasana) VALUES('Testi', 'Testi');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -17,8 +17,8 @@ nimi varchar(50) PRIMARY KEY
 );
 
 CREATE TABLE Luokitus(
-luokka INTEGER REFERENCES Luokka(nimi),
-tehtava INTEGER REFERENCES Tehtava(nimi)
+luokka varchar(50) REFERENCES Luokka(nimi),
+tehtava varchar(50) REFERENCES Tehtava(nimi)
 );
 
 CREATE TABLE Ylaluokka(
