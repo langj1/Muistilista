5208cc2ba53f40eb176993ba3a7fdf7afe73db11
Jimi <jimi8.lang@gmail.com>
2017-05-04 13:33:32 +0300

    korjaus

--- app/models/tehtava.php
+++ app/models/tehtava.php
@@ -16,7 +16,7 @@
 
    		$query = DB::connection()->prepare('SELECT * FROM Tehtava WHERE kayttaja = :kayttaja');
 
-   		$query->execute(array('id' => $tunnus));
+   		$query->execute(array('kayttaja' => $tunnus));
 
    		$rows = $query -> fetchAll();
 
